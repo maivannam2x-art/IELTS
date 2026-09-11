a49b230ef9968f74f3b6ebd961923958ba12b6fe
maivannam2x-art <maivannam2x@gmail.com>
2026-09-11 10:57:42 +0700

    feat: add service worker

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'ielts-vocab-v1';
+const CORE = ['/', '/manifest.webmanifest', '/icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
